79ca2e6d0da0a2e909b3da278ec4f0062473c2eb
olzhaskhamzin <olzhas.khamzin@gmail.com>
2023-10-03 22:23:44 +0600

    starting boilerplate of a test project

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="d-flex align-items-center justify-content-center flex-column w-100 h-100">
+      <h1>Home</h1>
+      <p>This is a test dashboard project.</p>
+    </div>
+  );
+};
+
+export default Home;
