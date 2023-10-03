2669aee6a7e5bc5671ed6f4d128ca9fec36c1397
olzhaskhamzin <olzhas.khamzin@gmail.com>
2023-10-03 22:25:48 +0600

    testing dev branch execution

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="d-flex align-items-center justify-content-center flex-column w-100 h-100">
+    <div className="d-flex align-items-center justify-content-center flex-column w-100">
       <h1>Home</h1>
       <p>This is a test dashboard project.</p>
     </div>
